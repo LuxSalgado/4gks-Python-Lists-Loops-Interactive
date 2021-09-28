45a8cb47879a9eeb7dc97935c3575e0c8b66646e
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-09-28 21:58:36 +0000

    Ejercicios resueltos hasta el número 5.1

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,5 +1,12 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
+def sumOdds(items):
+    odds = 0
+    for x in items:
+        if (x % 2 != 0):
+            odds = odds + x
+    return odds
 
+print(sumOdds(arr))
 
