4299a2c9b30edc9b588fd21878efd28a3b90d71a
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-09-28 21:50:53 +0000

    Ejercicios resueltos hasta el número 4.1

--- .learn/resets/04.1-count_on/app.py
+++ .learn/resets/04.1-count_on/app.py
@@ -3,10 +3,5 @@ my_list = [42, True, "towel", [2,1], 'hello', 34.4, {"name": "juan"}]
 
 
 #your code go here:
-hello = []
-
-for x in my_list:
-    if (type(x) == list or type(x) == dict):
-        hello.append(x)
 
 print(hello)
