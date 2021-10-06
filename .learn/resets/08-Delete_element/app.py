cb2050e704855b7381f4122d6046f9f9a7027d2a
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-10-06 23:30:51 +0000

    Ejercicios resueltos hasta el número 8

--- .learn/resets/08-Delete_element/app.py
+++ .learn/resets/08-Delete_element/app.py
@@ -3,12 +3,6 @@ people = ['juan','ana','michelle','daniella','stefany','lucy','barak']
 #Your code go here:
 def deletePerson(person_name):
     #Your code go here:
-    #nuevo = list(filter(lambda x : x != person_name, people))
-    nuevo = []
-    for x in people:
-        if x != person_name:
-            nuevo.append(x)
-    return nuevo
     
 print(deletePerson("daniella"))
 print(deletePerson("juan"))
