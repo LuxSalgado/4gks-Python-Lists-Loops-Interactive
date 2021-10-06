f7da521ee8d69a4a0220148438ff4eced6123f1f
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-10-06 23:37:07 +0000

    Ejercicios resueltos hasta el número 8.2

--- exercises/08.2-Divide_and_conquer/app.py
+++ exercises/08.2-Divide_and_conquer/app.py
@@ -2,6 +2,18 @@ list_of_numbers = [4,	80,	85,	59,	37, 25, 5, 64, 66, 81, 20, 64, 41, 22, 76, 76,
 
 
 #Your code here:
+def merge_two_list(numeros):
+    odd = []
+    even = []
+    for x in numeros:
+        if x % 2 == 0:
+            even.append(x)
+        else:
+            odd.append(x) 
+    juntos = []
+    juntos.append(odd)
+    juntos.append(even)
+    return juntos
 
 
 print(merge_two_list(list_of_numbers))
