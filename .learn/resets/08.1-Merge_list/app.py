9ff162412ba332ae1133125308940b6541c1563c
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-10-06 23:32:48 +0000

    Ejercicios resueltos hasta el número 8.1

--- .learn/resets/08.1-Merge_list/app.py
+++ .learn/resets/08.1-Merge_list/app.py
@@ -4,11 +4,5 @@ chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
 
 def merge_list(list1, list2):
     #Your code go here:
-    nuevo = []
-    for x in list1:
-        nuevo.append(x)
-    for x in list2:
-        nuevo.append(x)
-    return nuevo
     
 print(merge_list(chunk_one, chunk_two))
