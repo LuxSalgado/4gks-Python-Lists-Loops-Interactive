26ca3d42f5d919668c8ee1a27db482a6f850fe4a
LuxSalgado <lasalgado.14@est.ucab.edu.ve>
2021-10-09 21:44:29 +0000

    Ejercicios resueltos hasta el número 10

--- .learn/resets/10-Find_avg/app.py
+++ .learn/resets/10-Find_avg/app.py
@@ -1,7 +1,4 @@
 my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423]
 
 #Your code here:
-variable = 0
-for x in my_list:
-    variable = variable + x
-print(variable/len(my_list))
+
